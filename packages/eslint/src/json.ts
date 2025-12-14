c22420b2720e22a1311d0faf104b369e9becc53e
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-12-15 00:34:54 +0900

    fix json config

--- packages/eslint/src/json.ts
+++ packages/eslint/src/json.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from "eslint/config";
 import json from "@eslint/json";
 
-export const jsonConfig = defineConfig({
-  files: ["**/*.{json,jsonc,json5}"],
-  plugins: {
-    json,
+export const jsonConfig = defineConfig([
+  {
+    files: ["**/*.json"],
+    ignores: ["**/tsconfig.json", "**/tsconfig.*.json"],
+    plugins: {
+      json,
+    },
+    language: "json/json",
+    extends: ["json/recommended"],
   },
-  language: "json/json",
-  extends: ["json/recommended"],
-});
+  {
+    files: ["**/*.jsonc", "**/tsconfig.json", "**/tsconfig.*.json"],
+    plugins: {
+      json,
+    },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.json5"],
+    plugins: {
+      json,
+    },
+    language: "json/json5",
+    extends: ["json/recommended"],
+  },
+]);
